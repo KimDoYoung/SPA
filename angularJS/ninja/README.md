2fb0b76c94f9a0d365ec45bd57520321b56409ae
KimDoYoung <kdy987@gmail.com>
2022-05-14 16:26:06 +0900

    ninja

--- angularJS/ninja/README.md
+++ angularJS/ninja/README.md
@@ -5,3 +5,14 @@ Ninja angular tutorial
 
 - 6년전에 유튜브에서 강의한 것 들으면서 연습
 
+## 항목들
+
+- ng-src
+- ng-click
+- ng-view
+
+## router
+
+- jsonLint 사이트에서 json의 validation을 검사한다.
+- angular.toJson 
+
---
+++ angularJS/ninja/app/app.js
@@ -0,0 +1,45 @@
+var app = angular.module('app',['ngRoute', 'ngAnimate']);
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/home', {    
+            templateUrl : 'views/home.html',
+            controller : 'ninjaController'
+        })
+        .when('/list', {
+            templateUrl : 'views/list.html',
+            controller : 'ninjaController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+}]);
+app.directive('randomNinja', function(){
+    return {
+        restrict : 'E',
+        scope : {
+            ninjas : '=',
+            title : '='
+        },
+        replace : true,
+        transclue: true,
+        templateUrl : 'views/random.html',
+        controller : function($scope){
+            $scope.random = Math.floor(Math.random() * 4);
+        }
+    }
+})
+app.controller('ninjaController', ['$scope', '$http', function($scope, $http){
+    $scope.removeNinja = function(ninja){
+        $scope.ninjas.indexOf(ninja);
+        $scope.ninjas.splice(removeNinja,1);
+    };
+    $scope.addNinja = function(){
+        $scope.ninjas.push({
+            name: $scope.newninja.name,
+            belt: $scope.newninja.belt,
+            rate: parseInt($scope.newninja.rate, 10),
+            available: true
+        });
+    }
+
+}]);
