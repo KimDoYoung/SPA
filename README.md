9ba2c683288a12743f0bdd882817a38e041bcd86
KimDoYoung <kdy987@gmail.com>
2022-05-22 09:47:20 +0900

    0522

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-#SPA
+Single Page Application 관련 기술들
+==================================
+
+## angular개념
+
+![angular 개념](./images/angular1.png)
+
+## indexedDB
+![indexdb란](images/indexdb.png)
+![indexdb 개념](./images/indexdb1.png)
--- angularJS/ninja/app/app.js
+++ angularJS/ninja/app/app.js
@@ -1,37 +1,57 @@
 var app = angular.module('app',['ngRoute', 'ngAnimate']);
-app.config(['$routeProvider', function($routeProvider){
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    
+    //$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
     $routeProvider
-        .when('/home', {    
-            templateUrl : 'views/home.html', 
-            controller : 'ninjaController'
+        .when('/', {
+            templateUrl:'views/home.html',
+            controller: 'ninjaController'
         })
         .when('/list', {
             templateUrl : 'views/list.html',
             controller : 'ninjaController'
         })
+        .when('/contact', {
+            templateUrl : 'views/contact.html',
+            controller : 'ninjaController'
+        }) 
+        .when('/contact-success', {
+            templateUrl:'views/contact-success.html',
+            controller: 'ContactController'
+         })               
         .otherwise({
-            redirectTo: '/home'
+            redirectTo: '/'
         }); 
-}]);
-app.directive('randomNinja', function(){
-    return {
-        restrict : 'E',
-        scope : {
-            ninjas : '=',
-            title : '='
-        },
-        replace : true,
-        transclue: true,
-        templateUrl : 'views/random.html',
-        controller : function($scope){
-            $scope.random = Math.floor(Math.random() * 4);
-        }
+    }]);
+    app.directive('randomNinja', function(){
+        return {
+            restrict : 'E',
+            scope : {
+                ninjas : '=',
+                title : '='
+            },
+            replace : true,
+            transclude: true,
+            templateUrl : 'views/random.html',
+            controller : function($scope){
+                $scope.random = Math.floor(Math.random() * 4);
+            }
     }
-})
+});
+app.controller("ContactController",['$scope', '$location', function($scope, $location){
+    $scope.sendMessage = function(){
+        $location.path('/contact-success');
+    }
+}]);
 app.controller('ninjaController', ['$scope', '$http', function($scope, $http){
+
     $scope.removeNinja = function(ninja){
         $scope.ninjas.indexOf(ninja);
-        $scope.ninjas.splice(removeNinja,1);
+        $scope.ninjas.splice($scope.ninjas,1);
     };
     $scope.addNinja = function(){
         $scope.ninjas.push({
@@ -41,5 +61,16 @@ app.controller('ninjaController', ['$scope', '$http', function($scope, $http){
             available: true
         });
     }
+    $scope.removeAll = function(){
+        $scope.ninjas = [];
+    }
+    $http.get('data/ninjas.json').then(
+        (response)=>{
+            $scope.ninjas = response.data;
+        },
+        (error) => {
+            console.log(error)
+        }
+    );
 
 }]);
